2c2d82f919eb2e33eec27b78afbcbb33b9d4aa82
Ryoto Saito <ryoto.no.address@gmail.com>
2019-06-28 17:11:00 +0900

    update to use in AWS Lambda

--- sukkirisu.py
+++ sukkirisu.py
@@ -1,22 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import sys
+import json
+import urllib.parse
 import urllib.request
+import sys
 
 url = 'http://www.ntv.co.jp/sukkiri/sukkirisu/index.html'
 
-month = sys.argv[1]
+def lambda_handler(event, context):
+    month = urllib.parse.parse_qs(event['body'])['text'][0].rstrip()
+    result = get_sukkirisu(month)
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps({
+            "response_type": "in_channel",
+            "text": get_sukkirisu
+        })
+    }
 
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    body = res.read()
-    soup = BeautifulSoup(body, features="html.parser")
-    data = soup.find(id=("month"+str(month)))
-    if 'type1' in data['class']:
-        rank = "超スッキリす"
-    elif 'type4' in data['class']:
-        rank = "ガッカリす"
-    else:
-        rank = data.find("p", class_="rankTxt").text
-    print(rank + ": " + data.find("p", class_="").text + "\tラッキーカラー: " + data.find("div", id = "color").text)
+def get_sukkirisu(month):
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        soup = BeautifulSoup(body, features="html.parser")
+        data = soup.find(id=("month"+str(month)))
+        if 'type1' in data['class']:
+            rank = "超スッキリす"
+        elif 'type4' in data['class']:
+            rank = "ガッカリす"
+        else:
+            rank = data.find("p", class_="rankTxt").text
+        return rank + ": " + data.find("p", class_="").text + "\tラッキーカラー: " + data.find("div", id = "color").text
+
+if __name__ == '__main__':
+    print(get_sukkirisu(sys.argv[1]))
